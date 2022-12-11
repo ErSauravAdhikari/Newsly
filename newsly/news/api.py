dd3abacc871d92a0b75fc32c1d478f0c1afe10c1
Saurav Adhikari <ersauravadhikari@gmail.com>
2022-12-11 21:49:42 +0545

    Filter added to not show incomplete options

--- newsly/news/api.py
+++ newsly/news/api.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin
@@ -7,7 +8,7 @@ from .serializers import NewsSerializer
 
 
 class NewsViewSet(GenericViewSet, ListModelMixin):
-    queryset = News.objects.all().order_by('-created')
+    queryset = News.objects.filter(~Q(full_body_tts="")).filter(~Q(summary_tts="")).filter(~Q(summary="")).order_by('-created')
     serializer_class = NewsSerializer
     filter_backends = [
         DjangoFilterBackend
